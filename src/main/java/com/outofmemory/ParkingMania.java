0a29276b3e8c5d1086231f9ac50599b6e74768f5
Elya <elinafromua@gmail.com>
2021-01-12 22:02:33 +0200

    set port, add main method

--- src/main/java/com/outofmemory/ParkingMania.java
+++ src/main/java/com/outofmemory/ParkingMania.java
@@ -1,7 +1,11 @@
 package com.outofmemory;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ParkingMania {
     public static void main(String[] args) {
-
+        SpringApplication.run(ParkingMania.class, args);
     }
 }
